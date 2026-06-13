ee17e6fce270aa2525476445902a9ac1101d0e1f
Dhananjay <manedhanu@gmail.com>
2026-06-13 21:57:38 +0530

    remove cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5001;
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
